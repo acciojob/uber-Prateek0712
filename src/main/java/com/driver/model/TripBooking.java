9ee2277f24f8a7be01039b6830be5d54e5d44cf8
Pratik Jadhav <prateek7120@gmail.com>
2024-01-20 17:31:47 +0530

    3rd-Attempt

--- src/main/java/com/driver/model/TripBooking.java
+++ src/main/java/com/driver/model/TripBooking.java
@@ -42,58 +42,72 @@ public class TripBooking
         this.bill = bill;
     }
     public Driver getDriver() {
+
         return driver;
     }
 
     public void setDriver(Driver driver) {
+
         this.driver = driver;
     }
 
     public Customer getCustomer() {
+
         return customer;
     }
 
     public void setCustomer(Customer customer) {
+
         this.customer = customer;
     }
 
     public Integer getTripBookingId() {
+
         return tripBookingId;
     }
 
     public void setTripBookingId(Integer tripBookingId) {
+
         this.tripBookingId = tripBookingId;
     }
 
     public String getFromLocation() {
+
         return fromLocation;
     }
 
     public void setFromLocation(String fromLocation) {
+
         this.fromLocation = fromLocation;
     }
 
     public String getToLocation() {
+
         return toLocation;
     }
 
     public void setToLocation(String toLocation) {
+
         this.toLocation = toLocation;
     }
 
     public Integer getDistInKm() {
+
         return distInKm;
     }
 
     public void setDistInKm(Integer distInKm) {
+
         this.distInKm = distInKm;
     }
 
     public TripStatus getTripStatus() {
+
         return tripStatus;
     }
 
     public void setTripStatus(TripStatus tripStatus) {
+
         this.tripStatus = tripStatus;
     }
 
